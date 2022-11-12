f92d1c53857c93c9ad31c9b4a5e99d5e96127980
唐豪 <a2233192735>
2022-11-12 10:58:17 +0800

    最小硬币生成数改动态规划

--- java/priv/pront/code/dp/CoinsMin.java
+++ java/priv/pront/code/dp/CoinsMin.java
@@ -43,8 +43,8 @@ public class CoinsMin {
 
     public static int getMinCoins2(int[] coinsArray, int aim) {
         int[][] dp = new int[coinsArray.length + 1][aim + 1];
-        for (int i = 0; i < coinsArray.length; i++) {
-            for (int j = 0; j < aim; j++) {
+        for (int i = 0; i <= coinsArray.length; i++) {
+            for (int j = 0; j <= aim; j++) {
                 dp[i][j] = -2;
             }
         }
@@ -84,6 +84,44 @@ public class CoinsMin {
         return dp[index][rest];
     }
 
+    public static int getMinCoins3(int[] coinsArray, int aim) {
+        int N = coinsArray.length;
+        int[][] dp = new int[N + 1][aim + 1];
+
+        for (int row = 0; row <= N; row++) {
+            dp[row][0] = 0;
+        }
+        for (int col = 1; col <= aim; col++) {
+            dp[N][col] = -1;
+        }
+
+        for (int index = N - 1; index >= 0; index--) {
+            for (int rest = 1; rest <= aim; rest++) {
+
+//        rest > 0 并且 也有硬币
+                int p1 = dp[index + 1][rest];
+                int p2Next = -1;
+                if (dp[index + 1][rest - coinsArray[index]] >= 0) {
+                    p2Next = dp[index + 1][rest - coinsArray[index]];
+                }
+
+                if (p1 == -1 && p2Next == -1) {
+                    dp[index][rest] = -1;
+                } else {
+                    if (p1 == -1) {
+                        dp[index][rest] = p2Next + 1;
+                    }
+                    if (p2Next == -1) {
+                        dp[index][rest] = p1;
+                    }
+                    dp[index][rest] = Math.min(p1, p2Next + 1);
+                }
+            }
+        }
+        return dp[0][aim];
+    }
+
+
     // for test 对数器
     public static int[] generateRandomArray(int len, int max) {
         int[] arr = new int[(int) (Math.random() * len) + 1];
@@ -100,7 +138,7 @@ public class CoinsMin {
         for (int i = 0; i < testTime; i++) {
             int[] arr = generateRandomArray(len, max);
             int aim = (int) (Math.random() * 3 * max) + max;
-            if (getMinCoins1(arr, aim) != getMinCoins2(arr, aim)) {
+            if (getMinCoins1(arr, aim) != getMinCoins2(arr, aim) && getMinCoins2(arr, aim)!=getMinCoins3(arr,aim)) {
                 System.out.println("ooops!");
                 break;
             }
