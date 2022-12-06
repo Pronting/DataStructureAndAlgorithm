7f0321b9c38293d787f6f78e7513096778159c06
pronting <2233192735@qq.com>
2022-12-06 23:46:53 +0800

    矩阵的调度系列问题

---
+++ java/priv/pront/code/matrix/RotateMatrix.java
@@ -0,0 +1,51 @@
+package priv.pront.code.matrix;
+
+/**
+ * @Description: 给定一个正方形矩阵，只用有限几个变量，实现矩阵中每个位置的数顺时针转动
+ * 90度
+ * @Author: pront
+ * @Time:2022-12-06 23:19
+ */
+public class RotateMatrix {
+
+    public static void rotate(int[][] matrix) {
+        int tR = 0;
+        int tC = 0;
+        int dR = matrix.length - 1;
+        int dC = matrix[0].length - 1;
+        while (tR < dR) {
+            rotateEdge(matrix, tR++, tC++, dR--, dC--);
+        }
+    }
+
+    public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
+        int times = dC - tC;
+        int tmp = 0;
+        for (int i = 0; i != times; i++) {
+            tmp = m[tR][tC + i];
+            m[tR][tC + i] = m[dR - i][tC];
+            m[dR - i][tC] = m[dR][dC - i];
+            m[dR][dC - i] = m[tR + i][dC];
+            m[tR + i][dC] = tmp;
+        }
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i != matrix.length; i++) {
+            for (int j = 0; j != matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println("=========");
+        printMatrix(matrix);
+
+    }
+}
