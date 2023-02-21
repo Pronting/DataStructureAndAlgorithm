0cf0b096c5ef932096e6762bcb4b568466872952
pronting <2233192735@qq.com>
2023-02-21 16:50:46 +0800

    leetcode - 单调栈 去除重复字母

--- java/priv/pront/code/leetcode/mStack/L316_RemoveDuplicateLetters.java
+++ java/priv/pront/code/leetcode/mStack/L316_RemoveDuplicateLetters.java
@@ -1,5 +1,9 @@
 package priv.pront.code.leetcode.mStack;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+
 /**
  * @Description:
  * @Author: pront
@@ -7,7 +11,42 @@ package priv.pront.code.leetcode.mStack;
  */
 public class L316_RemoveDuplicateLetters {
 
-    public static void main(String[] args) {
-        System.out.println('a' -'b');
+    public String removeDuplicateLetters(String s) {
+
+        int[] lastIndex = new int[26];
+
+//  last index location of character
+        for(int i = 0; i< s.length(); i++){
+            lastIndex[s.charAt(i) - 'a'] = i;
+        }
+
+        Deque<Character> stack = new ArrayDeque<>();
+        // 添加哨兵节点
+        stack.addLast('a');
+        HashSet<Character> visited = new HashSet<>();
+
+        for(int i = 0 ; i < s.length(); i++){
+            char curChar = s.charAt(i);
+            // 说明之前放过
+            if(visited.contains(curChar)){
+                continue;
+            }
+
+//          后面还有相同子字母并且呈现单调递减
+            while(stack.peekLast() > curChar && lastIndex[stack.peekLast() - 'a'] > i){
+                visited.remove(stack.peekLast());
+                stack.pollLast();
+            }
+            visited.add(curChar);
+            stack.addLast(curChar);
+        }
+
+        stack.removeFirst();
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()){
+            sb.append(stack.pollFirst());
+        }
+        return sb.toString();
     }
 }
