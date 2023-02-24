3161ffd6bd72e352b02a2665e4904e6d6ef59436
pronting <2233192735@qq.com>
2023-02-24 17:30:42 +0800

    matrix中最大矩形面积

--- java/priv/pront/code/leetcode/matrix/L85_FindMax01Matrix.java
+++ java/priv/pront/code/leetcode/matrix/L85_FindMax01Matrix.java
@@ -1,5 +1,8 @@
 package priv.pront.code.leetcode.matrix;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  * @Description:
  * @Author: pront
@@ -7,5 +10,42 @@ package priv.pront.code.leetcode.matrix;
  */
 public class L85_FindMax01Matrix {
 
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int col = matrix.length;
+        int row = matrix[0].length;
+        int[] heights = new int[row];
+        int ans = 0;
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            ans = Math.max(ans, largestRectangleArea(heights));
+        }
+        return ans;
+    }
 
+    public int largestRectangleArea(int[] heights) {
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] new_heights = new int[heights.length + 2];
+        for (int i = 1; i < heights.length + 1; i++) {
+            new_heights[i] = heights[i - 1];
+        }
+
+        for (int i = 0; i < new_heights.length; i++) {
+            while (!stack.isEmpty() && new_heights[stack.peek()] > new_heights[i]) {
+                int cur = stack.pop();
+                res = Math.max(res, (i - stack.peek() - 1) * new_heights[cur]);
+            }
+            stack.push(i);
+        }
+        return res;
+    }
 }
