7b386cad8c2ae6630df18e4b6d47e7c90f979f9f
pronting <2233192735@qq.com>
2023-02-27 20:08:06 +0800

    leetcode hot100 72 ->编辑距离(dp)

---
+++ java/priv/pront/code/leetcode/dp/L72_EditDistance.java
@@ -0,0 +1,40 @@
+package priv.pront.code.leetcode.dp;
+
+/**
+ * @Description:
+ * @Author: pront
+ * @Time:2023-02-27 13:53
+ */
+public class L72_EditDistance {
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // 初始化状态
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        // 推导状态转移方程
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int insert = dp[i][j - 1] + 1;
+                    int delete = dp[i - 1][j] + 1;
+                    int replace = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+
+        // 最终结果
+        return dp[m][n];
+    }
+}
