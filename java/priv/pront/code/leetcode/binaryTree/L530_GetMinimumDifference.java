d5a539de3fc6d73393d2033dc60c42444cb3a4fd
tanghao <2233192735@qq.com>
2025-08-29 09:48:29 +0800

    feat:二叉搜索树的最小绝对差

---
+++ java/priv/pront/code/leetcode/binaryTree/L530_GetMinimumDifference.java
@@ -0,0 +1,32 @@
+package priv.pront.code.leetcode.binaryTree;
+
+import priv.pront.code.leetcode.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L530_GetMinimumDifference {
+
+    List<Integer> list = new ArrayList<>();
+
+    public int getMinimumDifference(TreeNode root) {
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < list.size(); i++){
+            min = Math.min(min, list.get(i) - list.get(i - 1));
+        }
+        return min;
+    }
+
+    private void dfs(TreeNode root){
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+    }
+
+
+    public static void main(String[] args) {
+        L530_GetMinimumDifference getMinimumDifference = new L530_GetMinimumDifference();
+        TreeNode testTreeNode = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(6));
+        System.out.println(getMinimumDifference.getMinimumDifference(testTreeNode));
+    }
+}
--- java/priv/pront/code/leetcode/utils/TreeNode.java
+++ java/priv/pront/code/leetcode/utils/TreeNode.java
@@ -11,7 +11,7 @@ public class TreeNode {
         public TreeNode right;
         TreeNode() {}
         public TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
+        public TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
